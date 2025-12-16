e1e53c79828810dfb783954e4a3828af5fac2ef1
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2025-12-16 02:24:34 +0100

    chore: update next config

--- apps/dash/next.config.ts
+++ apps/dash/next.config.ts
@@ -1,8 +1,14 @@
+import "@/lib/env";
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   output: "standalone",
+  transpilePackages: ["@t3-oss/env-nextjs", "@t3-oss/env-core"],
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
