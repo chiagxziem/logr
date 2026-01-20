6f6cca6a00abbe310401cdfd505770ba4b820d16
Chiagoziem Sunday <chiagoziem.ms@gmail.com>
2026-01-20 04:05:35 +0100

    chore: update api types

--- apps/api/src/types/api.ts
+++
@@ -1,5 +0,0 @@
-/** biome-ignore-all lint/complexity/noBannedTypes: temporal */
-export type AppEnv = {
-  Bindings: {};
-  Variables: {};
-};
--- apps/api/src/types/index.ts
+++ apps/api/src/types/index.ts
@@ -1,1 +1,4 @@
-export * from "./api";
+export type AppEnv = {
+  Bindings: {};
+  Variables: {};
+};
